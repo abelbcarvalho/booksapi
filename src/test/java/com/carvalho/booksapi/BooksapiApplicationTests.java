ddefa39b094bc0c4cb923b8c8316fe5cd3d3f079
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-03-20 10:42:07 -0300

    feat: project structure created spring boot

---
+++ src/test/java/com/carvalho/booksapi/BooksapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carvalho.booksapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

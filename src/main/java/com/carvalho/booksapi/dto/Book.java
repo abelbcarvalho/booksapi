dea2e382f97c9098fd706886257b2ac05332c515
Abel BC <160126794+abelbcarvalho@users.noreply.github.com>
2024-04-04 16:27:43 -0300

    feat: create model book class (#3)
    * feat: book model class created
    
    * feat: synopsis field create

---
+++ src/main/java/com/carvalho/booksapi/dto/Book.java
@@ -0,0 +1,20 @@
+package com.carvalho.booksapi.dto;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigInteger;
+
+@Getter
+@Setter
+@Data
+public class Book {
+    private BigInteger id = new BigInteger("0");
+    private string name;
+    private string author;
+    private string synopsis = null;
+    private string publisher = null;
+    private int numPages = 0;
+    private int yearPublish = 0;
+}
